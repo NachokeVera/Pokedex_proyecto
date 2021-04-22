af7f71193d38bcdfa453d3b7c48fe268bf738138
NachokeVera <ignacio.vera.hidalgo@gmail.com>
2021-04-22 17:20:11 -0400

    manipule el dom y agregar tinymc

--- js/index.js
+++ js/index.js
@@ -1,4 +1,25 @@
+
+tinymce.init({
+    selector: '#description-txt',
+    height: 200,
+    menubar: false,
+    plugins: [
+      'advlist autolink lists link image charmap print preview anchor',
+      'searchreplace visualblocks code fullscreen',
+      'insertdatetime media table paste code help wordcount'
+    ],
+    toolbar: 'undo redo | formatselect | ' +
+    'bold italic backcolor | alignleft aligncenter ' +
+    'alignright alignjustify | bullist numlist outdent indent | ' +
+    'removeformat | help',
+    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+  });
+
+
 document.querySelector("#registrar-btn").addEventListener("click", ()=>{
-     let nombre = document.querySelector("#nombre-txt").value;
-    alert("Chicas calientes a 2 kilimetros, que esperas!?" + nombre);
+    let nombre = document.querySelector("#nombre-txt").value;
+    let descripcion = tinymce.get("description-txt").getContent();
+    let legendario = document.querySelector("#legendario-si").checked;
+    let tipo = document.querySelector("#tipo-select").value;
+    console.log("wenasa", nombre,descripcion,legendario,tipo);
 });
