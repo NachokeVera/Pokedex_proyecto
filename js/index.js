154bee137494014ebccf9a0c44dc223773b7e770
NachokeVera <ignacio.vera.hidalgo@gmail.com>
2021-04-22 14:56:30 -0400

    Agregar tabla

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 document.querySelector("#registrar-btn").addEventListener("click", ()=>{
      let nombre = document.querySelector("#nombre-txt").value;
-    alert("Hola Mundo" + nombre);
+    alert("Chicas calientes a 2 kilimetros, que esperas!?" + nombre);
 });
